59de5200feff7bc3064b8c9695f8f87177d8f2b1
Tim-Alpha <codewithaisha@gmail.com>
2024-04-27 16:01:23 +0530

    update: add error handling & cors

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 import express from 'express';
 import db from './src/models/index.js';
+import cors from 'cors';
+import userRouter from './src/routers/user.js';
 
 const app = express();
 const port = 3001;
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-    db.sequelize.sync({ alter: true })
-        .then(() => {
-            console.log('Database synchronized successfully');
-        })
-        .catch(error => {
-            console.error('Error synchronizing database:', error);
-        });
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+const v1 = '/api/v1';
+
+app.use(v1, userRouter);
+
+// Error handling middleware should be placed after all other middleware/route usage.
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
+
+// Async function to handle DB sync and server start
+const startServer = async () => {
+    try {
+        await db.sequelize.sync({ alter: true });
+        console.log('Database synchronized successfully');
+        app.listen(port, () => {
+            console.log(`Server is running on http://localhost:${port}`);
+        });
+    } catch (error) {
+        console.error('Error synchronizing database:', error);
+    }
+}
+
+startServer();
