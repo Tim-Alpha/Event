32b9237a18f83cedaea67557ffb783e8cdd86677
Tim-Alpha <codewithaisha@gmail.com>
2024-04-28 14:46:44 +0530

    update: add venue router

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import db from './src/models/index.js';
 import cors from 'cors';
 import userRouter from './src/routers/user.js';
+import venueRouter from './src/routers/venue.js';
 
 const app = express();
 const port = 3001;
@@ -11,6 +12,7 @@ app.use(express.json());
 const v1 = '/api/v1';
 
 app.use(v1, userRouter);
+app.use(v1, venueRouter);
 
 // Error handling middleware should be placed after all other middleware/route usage.
 app.use((err, req, res, next) => {
@@ -21,7 +23,8 @@ app.use((err, req, res, next) => {
 // Async function to handle DB sync and server start
 const startServer = async () => {
     try {
-        // await db.sequelize.sync({ alter: true });
+        // await db.sequelize.sync({alter: true});
+        await db.sequelize.sync();
         console.log('Database synchronized successfully');
         app.listen(port, () => {
             console.log(`Server is running on http://localhost:${port}`);
