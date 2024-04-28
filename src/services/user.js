cca3a2dc34313d17e701b2c4bde75a5fbee6fa4c
Tim-Alpha <codewithaisha@gmail.com>
2024-04-28 14:51:16 +0530

    update: attach venue data to users

--- src/services/user.js
+++ src/services/user.js
@@ -12,7 +12,13 @@ const createUser = async (userData) => {
 
 const getAllUsers = async () => {
     try {
-        const users = await User.findAll();
+        const users = await User.findAll({
+            include: [{
+                model: db.Venue,
+                as: 'venues'
+            }]
+        });
+
         return users;
     } catch (error) {
         throw new Error('Error in getting all users: ' + error.message);
