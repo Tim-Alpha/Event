a22ed297bc89a49b03037adb1f55f7e1afe0675f
Tim-Alpha <codewithaisha@gmail.com>
2024-05-11 22:12:17 +0530

    update: implement generate otp function

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,3 +1,30 @@
+import crypto from 'crypto';
+
+const padOTP = (num) => num.toString().padStart(4, '0');
+
+const generateOTP = (mobileNumber) => {
+    const secret = process.env.OTP_SECRET;
+    if (!secret) {
+        throw new Error("Secret key for OTP generation is not defined.");
+    }
+
+    // Introduce a time factor that changes every 5 minute
+    const timeFactor = Math.floor(Date.now() / (1000 * 60 * 5)); // OTP expired after every 5 minutes
+    const data = String(mobileNumber) + String(timeFactor);
+
+    const hash = crypto.createHmac('sha256', secret)
+        .update(String(data))
+        .digest('hex');
+    
+    const otp = parseInt(hash.substring(hash.length - 4), 16).toString().substring(0, 4);
+    return padOTP(otp);
+};
+
+const verifyOTP = (mobileNumber, inputOTP) => {
+    const regeneratedOTP = generateOTP(mobileNumber);
+    return regeneratedOTP === inputOTP;
+};
+
 const response = (status, message, dataName, data, pageNumber=null, maxPageSize=null, pageSize=null) => {
     if (!pageNumber) {
         return {
@@ -16,4 +43,4 @@ const response = (status, message, dataName, data, pageNumber=null, maxPageSize=
     };
 }
 
-export default response;
+export { generateOTP, verifyOTP, response };
