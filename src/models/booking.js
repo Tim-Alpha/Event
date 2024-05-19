11ae99ada25a2b8139e0a0819af016e2d30b9042
Tim-Alpha <codewithaisha@gmail.com>
2024-05-19 09:17:54 +0530

    update: implement booking model

--- src/models/booking.js
+++ src/models/booking.js
@@ -1,28 +1,53 @@
 import { DataTypes } from "sequelize";
 
-const Booking = ( sequelize ) => {
+const Booking = (sequelize) => {
     const Booking = sequelize.define("Booking", {
         uuid: {
             type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false
         },
         status: {
             type: DataTypes.STRING(30),
-            allowNull: false
+            allowNull: false,
+            field: 'status'
         },
         bookingDate: {
             type: DataTypes.DATE,
-            allowNull: false
+            allowNull: false,
+            field: 'booking_date'
         }
     }, {
         tableName: "bookings",
         underscored: true,
-        paranoid: true
+        paranoid: true,
+        hooks: {
+            afterCreate: async (booking, options) => {
+                try {
+                    const user = await booking.getUser();
+                    const venue = await booking.getVenue();
+                    booking.setDataValue('user', user);
+                    booking.setDataValue('venue', venue);
+                } catch (error) {
+                    console.error('Error in fetching owner data: ' + error);
+                }
+            }
+        }
     });
 
+    Booking.prototype.toJSON = function () {
+        let attributes = Object.assign({}, this.get());
+        delete attributes.id;
+        delete attributes.deletedAt;
+        delete attributes.userId;
+        delete attributes.venueId;
+
+        return attributes;
+    }
+
     Booking.associate = (models) => {
-        Booking.belongsTo(models.User, { foreignKeys: 'userId', as: 'user', onDelete: 'CASCADE' });
-        Booking.belongsTo(models.Event, { foreignKeys: 'eventId', as: 'event', onDelete: 'CASCADE' });
+        Booking.belongsTo(models.User, { foreignKey: 'userId', as: 'user', onDelete: 'CASCADE' });
+        Booking.belongsTo(models.Event, { foreignKey: 'venueId', as: 'venue', onDelete: 'CASCADE' });
     };
 
     return Booking;
